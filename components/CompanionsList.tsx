0f94ac243443b0e8ed13453d91b594359a358dc1
Nilangi-Edirisinghe <nilangi.ediri@gmail.com>
2025-10-24 21:16:23 +1030

    Created Table with UI enhancements in CompanionsList Component

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -10,6 +10,8 @@ import {
 
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import Image from "next/image";
+import { getSubjectColor } from "@/assets/lib/utils";
 
 interface CompanionsListProps {
     title: string,
@@ -31,13 +33,51 @@ const CompanionsList = ({ title, companions, classNames }: CompanionsListProps) 
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {companions?.map((companion) => (
-                        <TableRow>
+                    {companions?.map(({ id, subject, name, topic, duration }) => (
+                        <TableRow key={id}>
                             <TableCell>
-                                <Link href={`/companions/${companion.id}`}>
-                                    {companion.subject}
+                                <Link href={`/companions/${id}`}>
+                                    <div className="flex items-center gap-2">
+                                        <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{ backgroundColor: getSubjectColor(subject) }}>
+                                            <Image src={`/icons/${subject}.svg`}
+                                                alt={subject}
+                                                width={35}
+                                                height={35} />
+                                        </div>
+                                        <div className="flex flex-col gap-2">
+                                            <p className="font-bold text-2xl">
+                                                {name}
+
+                                            </p>
+                                            <p className="text-lg">
+                                                {topic}
+                                            </p>
+                                        </div>
+                                    </div>
                                 </Link>
                             </TableCell>
+                            <TableCell>
+                                <div className="subject-badge w-fit max-md:hidden">
+                                    {subject}
+                                </div>
+                                <div className="flex items-center justify-center rounded-lg w-fit p-2 md:hidden" style={{ backgroundColor: getSubjectColor(subject) }}>
+                                    <Image src={`/icons/${subject}.svg`}
+                                        alt={subject}
+                                        width={18}
+                                        height={18}
+                                    />
+                                </div>
+                            </TableCell>
+                            <TableCell>
+                                <div className="flex items-center gap-2 w-full justify-end">
+                                    <p className="text-xl">
+                                        {duration}{''}
+                                        <span className="max-md:hidden"> mins</span>
+                                    </p>
+                                    <Image src="/icons/clock.svg" alt="minutes" width={14} height={14} className="md:hidden" />
+                                </div>
+                            </TableCell>
+
                         </TableRow>
                     ))}
 
