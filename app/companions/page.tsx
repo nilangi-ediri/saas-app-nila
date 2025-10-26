1a68f69da13d5b4f4c3e80bc8dbbff273326a8c1
Nilangi-Edirisinghe <nilangi.ediri@gmail.com>
2025-10-26 19:36:37 +1030

    Getting Companions to Frontend from Backend

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,30 @@
-const CompanionsLibrary = () => {
+import { getAllCompanions } from "@/assets/lib/actions/companion.actions";
+import { getSubjectColor } from "@/assets/lib/utils";
+import CompanionCard from "@/components/CompanionCard";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+    const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : '';
+    const topic = filters.topic ? filters.topic : '';
+    // console.log(subject, topic)
+    const companions = await getAllCompanions({ subject, topic })
+    // console.log(companions)
+
+
     return (
-        <div>CompanionsLibrary</div>
+        <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">Filters</div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard key={companion.id} {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
     )
 }
 
