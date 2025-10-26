924c5616b2942f868753daa22821661c97d59552
Nilangi-Edirisinghe <nilangi.ediri@gmail.com>
2025-10-27 08:24:29 +1030

    Updated Search Input component to work properly with new packaged installation and delay added

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,9 @@
 'use client';
 import Image from 'next/image';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { formUrlQuery } from '@jsmastery/utils';
+import { removeKeysFromUrlQuery } from '@jsmastery/utils';
 
 const SearchInput = () => {
     const pathname = usePathname();
@@ -9,6 +11,31 @@ const SearchInput = () => {
     const searchParams = useSearchParams();
     const query = searchParams.get('topic') || '';
     const [searchQuery, setSearchQuery] = useState('');
+
+    useEffect(() => {
+        const delayDebouncefn = setTimeout(() => {
+            if (searchQuery) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: "topic",
+                    value: searchQuery,
+                });
+
+                router.push(newUrl, { scroll: false });
+            } else {
+                if (pathname == '/companions') {
+                    const newUrl = removeKeysFromUrlQuery({
+                        params: searchParams.toString(),
+                        keysToRemove: ["topic"],
+                    });
+                    router.push(newUrl, { scroll: false });
+                }
+            }
+        }, 500)
+
+
+
+    }, [searchQuery, router, searchParams, pathname])
 
     return (
         <div className='relative border border-black rounded-lg items-center flex-gap-2 px-2 py-1 h-fit'>
