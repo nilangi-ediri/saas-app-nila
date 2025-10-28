4fdca50461a3796361f4a0d457c23f94b18593d0
Nilangi-Edirisinghe <nilangi.ediri@gmail.com>
2025-10-28 11:13:11 +1030

    Connected Vapi and created server logic to retrieve only one companion

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,4 +1,19 @@
-const CompanionSession = () => {
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getCompanion } from "@/assets/lib/actions/companion.actions";
+
+interface CompanionSessionPageProps {
+    params: Promise<{ id: string }>;
+}
+
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+    const { id } = await params;
+    const companion = await getCompanion(id);
+    const user = await currentUser();
+
+    if (!user) redirect('/sign-in');
+    if (!companion) redirect('/companions')
+
     return (
         <div>CompanionSession</div>
     )
